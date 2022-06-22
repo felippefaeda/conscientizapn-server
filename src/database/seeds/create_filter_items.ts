258c28e2634e093c193e40284d666f98f7a76400
João Pedro Gaudencio <jp.goesvieira@gmail.com>
2022-06-22 02:36:12 -0300

    atualizando dados web-server

--- src/database/seeds/create_filter_items.ts
+++ src/database/seeds/create_filter_items.ts
@@ -1,10 +1,11 @@
 import {Knex} from 'knex';
 export async function seed(knex: Knex){
     await knex('items').insert([
-        {id:1, title: 'Plástico'},
-        {id:2, title: 'Papel'},
-        {id:3, title: 'Óleo'},
-        {id:4, title: 'Eletônico'},
-        {id:5, title: 'Pilhas'},
+        {id:1, title: 'Orgânico', image:'organicos.svg'},
+        {id:2, title: 'Papel', image:'papeis-papelao.svg'},
+        {id:3, title: 'Óleo', image:'oleo.svg'},
+        {id:4, title: 'Eletrônico', image:'eletronicos.svg'},
+        {id:5, title: 'Pilhas', image:'baterias.svg'},
+        {id:6, title: 'Lâmpadas', image:'lampadas.svg'},
     ]);
 }
--- src/server.ts
+++ src/server.ts
@@ -19,4 +19,6 @@ app.use(
 
 app.use(routes);
 
+app.use('/uploads', express.static(path.resolve(__dirname,'..','uploads')));
+
 app.listen(3333);
